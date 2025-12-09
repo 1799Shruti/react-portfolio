53fd02efb9cfd1f7e2881315a793bfc8f1d0be6b
Shruti Bhandari <shruti.bhandari@centralogic.net>
2025-12-09 14:55:48 +0530

    Color Clocks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 import StudentTable from "./Table";
+import DigitalClock from "./Clock";
+
 function App() {
   // const [count, setCount] = useState(0)
   var [val, setVal] = useState();
+  const colors = [
+    "Red",
+    "Blue",
+    "Green",
+    "Yellow",
+    "Black",
+    "Purple",
+    "Orange",
+  ];
+
+  const [selectedColor, setSelectedColor] = useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setSelectedColor(value);
+    console.log("Selected Color:", value); // print value
+  };
   return (
     <div>
       <h1>Hello</h1>
@@ -16,8 +35,23 @@ function App() {
       >
         Clear Input
       </button>
+      <div style={{ padding: "20px" }}>
+        <h3>Select a Color</h3>
 
+        <select value={selectedColor} onChange={handleChange}>
+          <option value="">-- Choose Color --</option>
+
+          {colors.map((color, index) => (
+            <option key={index} value={color}>
+              {color}
+            </option>
+          ))}
+        </select>
+
+        <h4>Selected Color: {selectedColor}</h4>
+      </div>
       <StudentTable></StudentTable>
+      <DigitalClock color={selectedColor}></DigitalClock>
     </div>
   );
 }
