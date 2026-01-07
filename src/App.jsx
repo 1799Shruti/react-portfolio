e12f64504e8447e1e68143ea450f69fc6aca6f63
Shruti Bhandari <shruti.bhandari@centralogic.net>
2026-01-07 15:10:26 +0530

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import "./App.css";
 import StudentTable from "./Table";
 import DigitalClock from "./Clock";
+import { BrowserRouter, Routes, Route, Link } from "react-router";
 
 function App() {
-  // const [count, setCount] = useState(0)
-  var [val, setVal] = useState();
+  const [val, setVal] = useState("");
   const colors = [
     "Red",
     "Blue",
@@ -15,44 +15,56 @@ function App() {
     "Purple",
     "Orange",
   ];
-
   const [selectedColor, setSelectedColor] = useState("");
 
   const handleChange = (e) => {
     const value = e.target.value;
     setSelectedColor(value);
-    console.log("Selected Color:", value); // print value
+    console.log("Selected Color:", value);
   };
+
   return (
-    <div>
-      <h1>Hello</h1>
-      <input type="text" value={val} onChange={(e) => setVal(e.target.value)} />
-      <h1>{val}</h1>
-      <button
-        onClick={() => {
-          setVal("");
-        }}
-      >
-        Clear Input
-      </button>
-      <div style={{ padding: "20px" }}>
-        <h3>Select a Color</h3>
+    <BrowserRouter>
+      <div>
+        <h1>Hello</h1>
+        <input
+          type="text"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <h1>{val}</h1>
+        <button onClick={() => setVal("")}>Clear Input</button>
 
-        <select value={selectedColor} onChange={handleChange}>
-          <option value="">-- Choose Color --</option>
+        <div style={{ padding: "20px" }}>
+          <h3>Select a Color</h3>
+          <select value={selectedColor} onChange={handleChange}>
+            <option value="">-- Choose Color --</option>
+            {colors.map((color, index) => (
+              <option key={index} value={color}>
+                {color}
+              </option>
+            ))}
+          </select>
+          <h4>Selected Color: {selectedColor}</h4>
+        </div>
 
-          {colors.map((color, index) => (
-            <option key={index} value={color}>
-              {color}
-            </option>
-          ))}
-        </select>
+        <StudentTable />
+        <DigitalClock color={selectedColor} />
 
-        <h4>Selected Color: {selectedColor}</h4>
+        <div>
+          <Link to="/">Home</Link> <br />
+          <Link to="/about">About Us</Link>
+          <br />
+          <Link to="/contact">Contact Us</Link>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About Us</h1>} />
+          <Route path="/contact" element={<h1>Contact Us</h1>} />
+        </Routes>
       </div>
-      <StudentTable></StudentTable>
-      <DigitalClock color={selectedColor}></DigitalClock>
-    </div>
+    </BrowserRouter>
   );
 }
 
